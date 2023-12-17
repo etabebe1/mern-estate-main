b06bbe4910c8c5063bb99f470ea91ae50d5ac1f9
etabebe1 <etabebe1@gmail.com>
2023-12-17 10:11:55 -0500

    add, delete, and make realtime db connection with onSnapshot()

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  // NOTE: onSnapshot function helps us to make realtime db connection bellow
+  onSnapshot,
+} from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -31,10 +40,64 @@ const colRef = collection(DB, "collectionName");
 
 // REMARK: get collection data ---> get all the documents in the specified ((collectionName)) above
 // NOTE: getDocs() ---> function to get data
-//* getDocs(colRef).then((snapshot) => console.log(snapshot));
+// * The function bellow always fires once but to make a realtime db connection, onSnapshot() function will be needed
+try {
+  let collectionData = [];
+  const snapshot = await getDocs(colRef);
+  snapshot.docs.forEach((doc) => {
+    collectionData.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(collectionData);
+} catch (error) {
+  console.log(error.message);
+}
+
+//* REMARK: realtime db connection in firebase
+onSnapshot(colRef, (snapshot) => {
+  try {
+    let collectionData = [];
+    snapshot.docs.forEach((doc) => {
+      collectionData.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(collectionData);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 // REMARK: Add Data ---> add new Data to existing collection in this case to ((the collectionName)) above
 // NOTE: addDoc () ---> function to add data
+const handleAddData = async (evt) => {
+  evt.preventDefault();
+  try {
+    const dataAdded = await addDoc(colRef, {
+      // NOTE: doc property || field goes here bellow
+      // NOTE: values can be an input value from client side.
+      field1: "value1",
+      field2: "value2",
+    });
+
+    console.log(`Document added with ID: ${dataAdded.id}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 // REMARK: Delete Data ---> delete a Data from existing collection in this case to ((the collectionName)) above
 // NOTE: deleteDoc () ---> function to delete data in the collection
+
+const handleDeleteData = async (evt) => {
+  // NOTE: for react component we can use useRef() ---> hook to grab current value
+  const idValueFromInput = "id value of data to be deleted"; // = useRef()
+
+  evt.preventDefault();
+  try {
+    const dataDeleted = doc(DB, "collectionNameHere", idValueFromInput);
+    //* The above code refers to a specific data in the collection of firebase DB
+
+    deleteDoc(dataDeleted);
+    console.log(`Document added with ID: ${dataDeleted.id}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
