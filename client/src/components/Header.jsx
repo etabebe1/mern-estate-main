9839258d45d4e8c6167dacd2b308a86027517386
etabebe1 <etabebe1@gmail.com>
2024-01-07 02:15:18 -0500

    search functionality completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import Person from "@mui/icons-material/Person";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
   // LOGS:
   // console.log(currentUser.user);
   // console.log(PF + currentUser.user.avatar);
+  console.log(searchTerm);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFormUrl = urlParams.get("searchTerm");
+    console.log(searchTermFormUrl);
+    searchTermFormUrl && setSearchTerm(searchTermFormUrl);
+  }, []);
 
   return (
     <header className="bg-black shadow-lg">
@@ -20,16 +38,23 @@ export default function Header() {
             <span className="text-slate-300">Estate</span>
           </h1>
         </Link>
-        <form>
-          <div className="flex items-center justify-between rounded bg-slate-200 px-3 py-0 ">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="w-28 bg-transparent sm:w-52 lg:w-80 focus:outline-none text-zinc-900 text-sm md:text-base md:py-1"
-            />
-            <SearchIcon className="text-zinc-900 cursor-pointer font-light " />
-          </div>
+
+        <form
+          className="flex items-center justify-between rounded bg-slate-200 px-3 py-0"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-28 bg-transparent sm:w-52 lg:w-80 focus:outline-none text-zinc-900 text-sm md:text-base md:py-1"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <SearchIcon className="text-zinc-900 cursor-pointer font-light" />
+          </button>
         </form>
+
         <ul className="text-slate-300 flex justify-center items-end gap-4 text-sm md:text-base">
           <li className="hidden sm:inline hover:underline transition duration-500">
             <Link to={"/"}>Home</Link>
