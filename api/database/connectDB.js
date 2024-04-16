00ccd802c59b6917dac795f3bdf82e56fd3c18e3
etabebe1 <etabebe1@gmail.com>
2024-04-16 17:10:44 -0400

    db connection updated

--- api/database/connectDB.js
+++ api/database/connectDB.js
@@ -4,8 +4,9 @@ const connectDB = (url) => {
   return mongoose
     .connect(url)
     .then(() => console.log("Successfully connected to MongoDB \u{2714}"))
-    .catch(() => {
-      console.log(error);
+    .catch((error) => {
+      // Correctly catch the error object here
+      console.error("Failed to connect to MongoDB:", error);
     });
 };
 
